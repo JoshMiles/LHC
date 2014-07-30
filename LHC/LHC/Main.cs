58abcffd84b0ca8a62a1866775838400e64f45a7
SplittyDev <splittydev@gmail.com>
2014-07-30 03:01:51 +0200

    Started processing arguments

--- LHC/LHC/Main.cs
+++ LHC/LHC/Main.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Collections.Generic;
 using Mono.Cecil;
 
 namespace LHC
@@ -12,19 +14,51 @@ namespace LHC
 				BanWitches ();
 			}
 
+			// Initialize start parameters with default values
+			StartParams sp = new StartParams () {
+				input = new List<string> (),
+				output = "kernel.iso",
+				entry = "auto",
+				verbose = false,
+			};
+
+			// Check for specific arguments
 			for (int i = 0; i < args.Length; i++) {
 				switch (args[i])
 				{
 					case "-i":
 					case "--include":
+						if (args.Length < ++i || !File.Exists (args[i]))
+						{
+							Console.WriteLine ("Path expected. U mad bro?");
+							BanWitches ();
+						}
+						sp.input.Add (Path.GetFullPath (args[i]));
 						break;
 
 					case "-e":
 					case "--entry":
+						if (args.Length < ++i)
+						{
+							Console.WriteLine ("String expected. U mad bro?");
+							BanWitches ();
+						}
+						sp.entry = args[i];
 						break;
 
 					case "-o":
 					case "--output":
+						if (args.Length < ++i || !File.Exists (args[i]))
+						{
+							Console.WriteLine ("Path expected. U mad bro?");
+							BanWitches ();
+						}
+						sp.output = Path.GetFullPath (args[i]);
+						break;
+					
+					case "-v":
+					case "--verbose":
+						sp.verbose = true;
 						break;
 
 					default:
@@ -35,6 +69,9 @@ namespace LHC
 			}
 		}
 
+		/// <summary>
+		/// Bans the witches.
+		/// </summary>
 		public static void BanWitches ()
 		{
 			Console.WriteLine ("Usage: lhc -i kernel.dll [-i witch.dll] -e Kernel.Kernel.Main -o kernel.iso\n");
@@ -43,6 +80,7 @@ namespace LHC
 			Console.WriteLine ("{0}\t\t{1}\t\t{2}", "-i", "--include", "Include DLL");
 			Console.WriteLine ("{0}\t\t{1}\t\t{2}", "-e", "--entry  ", "Main entry point");
 			Console.WriteLine ("{0}\t\t{1}\t\t{2}", "-o", "--output ", "Output file");
+			Console.WriteLine ("{0}\t\t{1}\t\t{2}", "-v", "--verbose", "Detailled logging");
 			Console.WriteLine ("".PadLeft (Console.WindowWidth, '-'));
 			Console.WriteLine ("\nNote: The first DLL to include must be the kernel!");
 			Environment.Exit (0);
---
+++ LHC/LHC/StartParams.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace LHC
+{
+	/// <summary>
+	/// Start parameters.
+	/// </summary>
+	public struct StartParams
+	{
+		public List<string> input;
+		public string output;
+		public string entry;
+		public bool verbose;
+	}
+}
+
